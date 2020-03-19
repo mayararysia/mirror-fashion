418ccfde07c40d096e8b3d841cb823e1fe70f72f
Mayara Rysia <mayara.ryzia@gmail.com>
2020-03-18 21:08:15 -0300

    chapter 10

---
+++ js/home.js
@@ -0,0 +1,18 @@
+$('.painel button').click(function() {
+    $(this).parent().toggleClass('painel-aberto');
+});
+
+// $('.novidades button').click(function() {
+//     $('.novidades').toggleClass('painel-aberto');
+// });
+
+// $('.mais-vendidos button').click(function() {
+//     $('.mais-vendidos').toggleClass('painel-aberto');
+// });
+
+// $(document).ready(function() {
+//     console.log("teste");
+//     $('.novidades button').click(function() {
+//         //$('.novidades').addClass('painel-aberto');
+//     });
+// });
